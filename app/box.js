5caaf350e8e66a14f0b1ddb4ac0bfaf38fabe29e
hecarim <hecarim@pop-os.localdomain>
2024-01-17 13:58:51 +0530

    image fade commit

--- app/box.js
+++ app/box.js
@@ -1,24 +1,40 @@
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 import { createElement, useState } from "react"
 export default function Box(props) {
-    return (
-    <>
-    <motion.div
-            animate={{ y: -props.tester}}
-            transition={{ type: "linear", duration: 10}} >
-    {Listofimg(props.url)}
-    </motion.div>
-    </>
-    )
+
+	return (
+		<>
+			<motion.div
+				animate={{ y: -props.tester }}
+				transition={{ type: "linear", duration: 10 }} >
+				{Listofimg(props.url)}
+			</motion.div>
+		</>
+	)
 }
 
 function Listofimg(ele) {
-    return (ele.map((url,index) => (
-        createImage(url,index)
-    )))
-
+	return (
+		<>
+			{Array.from(Object.keys(ele), (x) => {
+				return createImage(ele[x])
+			})}
+		</>)
 }
 
-function createImage(url, index) {
-    return <img src={url} key={index} alt="missing" ></img>
+function createImage(data) {
+	const variants = {
+		closed: { opacity: 0, },
+		open: { opacity: 1, },
+	}
+	return (
+		<motion.div animate={data.anistate.display ? "open" : "closed"}
+			variants={variants}>
+			<img src={data.sr} alt="missing" style={{
+				position: 'absolute',
+				top: `${data.t}px`,
+				left: `${data.l}px`,
+			}}>
+			</img>
+		</motion.div>)
 }
