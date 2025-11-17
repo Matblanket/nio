d3386a2f1c16075c3149b539dbd33f2a976d066e
Matblanket <anosh.k.v@gmail.com>
2025-11-16 21:43:39 -0500

    reverting

--- pynio/helper.py
+++
@@ -1,4 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="4uLPYt53nLEqtVOxpRq0")
-project = rf.workspace("personal-ov9jg").project("comic-panel-detectors")
-dataset = project.version(8).download("coco")
